8cb1f0d8069c45c7ff4e1d8964bc281f42602f56
AGarcia <you@example.com>
2020-11-01 09:46:51 -0800

    registered Project model in order to use admin interface in browser

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
+
 from django.contrib import admin
+# import Projects model in order to register it
+from projects.models import Project
 
 # Register your models here.
+# pass in the name of the imported model ("Project")
+admin.site.register(Project)
